1b95cc473ca13fcba834ab055e93de0f0ab71651
QuantumXenon <edwinza31415@gmail.com>
2022-09-01 01:32:33 +0100

    Fixed import

--- src/main/java/tfar/xlpackets/mixin/ServerboundCustomPayloadPacketMixin.java
+++ src/main/java/tfar/xlpackets/mixin/ServerboundCustomPayloadPacketMixin.java
@@ -1,6 +1,6 @@
 package tfar.xlpackets.mixin;
 
-import net.minecraft.network.play.server.SCustomPayloadPlayPacket;
+import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
