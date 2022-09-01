13885e5cac262a59a1e7384406b60197427965c9
QuantumXenon <edwinza31415@gmail.com>
2022-09-01 02:13:07 +0100

    Change class

--- src/main/java/tfar/xlpackets/mixin/ClientboundCustomPayloadPacketMixin.java
+++ src/main/java/tfar/xlpackets/mixin/ClientboundCustomPayloadPacketMixin.java
@@ -1,12 +1,12 @@
 package tfar.xlpackets.mixin;
 
-import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
+import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(ServerboundCustomPayloadPacket.class)
-public class ServerboundCustomPayloadPacketMixin {
+@Mixin(ClientboundCustomPayloadPacket.class)
+public class ClientboundCustomPayloadPacketMixin {
 
     @ModifyConstant(method = {"getInternalData"},constant = @Constant(intValue = 1048576))
     private int xlPackets(int constant) {
