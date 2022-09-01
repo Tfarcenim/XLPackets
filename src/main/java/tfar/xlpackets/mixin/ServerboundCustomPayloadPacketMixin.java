c4125c41d5176548f614073edc01c12bb3752155
QuantumXenon <edwinza31415@gmail.com>
2022-09-01 01:28:38 +0100

    Updated method and classes

--- src/main/java/tfar/xlpackets/mixin/ServerboundCustomPayloadPacketMixin.java
+++ src/main/java/tfar/xlpackets/mixin/ServerboundCustomPayloadPacketMixin.java
@@ -1,14 +1,14 @@
 package tfar.xlpackets.mixin;
 
-import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+import net.minecraft.network.play.server.SCustomPayloadPlayPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(ClientboundCustomPayloadPacket.class)
-public class SCustomPayloadPacketMixin {
+@Mixin(ServerboundCustomPayloadPacket.class)
+public class ServerboundCustomPayloadPacketMixin {
 
-    @ModifyConstant(method = {"handle"},constant = @Constant(intValue = 1048576))
+    @ModifyConstant(method = {"getInternalData"},constant = @Constant(intValue = 1048576))
     private int xlPackets(int constant) {
         return 2000000000;
     }
