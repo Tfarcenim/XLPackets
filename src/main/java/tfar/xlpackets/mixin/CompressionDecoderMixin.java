e1336a464c9b2431a585715f81d4b2e8af54ddb2
QuantumXenon <edwinza31415@gmail.com>
2022-09-01 20:53:32 +0100

    Update CompressionDecoderMixin.java

--- src/main/java/tfar/xlpackets/mixin/CompressionDecoderMixin.java
+++ src/main/java/tfar/xlpackets/mixin/CompressionDecoderMixin.java
@@ -7,8 +7,8 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(CompressionDecoder.class)
 public class CompressionDecoderMixin {
-	@ModifyConstant(method = "decode",constant = @Constant(intValue = 2097152))
-	private int xlPackets(int old) {
-		return 2000000000;
-	}
+    @ModifyConstant(method = "decode", constant = @Constant(intValue = 2097152))
+    private int xlPackets(int old) {
+        return 2000000000;
+    }
 }
