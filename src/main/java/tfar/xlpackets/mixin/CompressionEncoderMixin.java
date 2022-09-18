80bf77625137f0e1da426e4c6636322a794fc906
QuantumXenon <edwinza31415@gmail.com>
2022-09-18 14:05:34 +0100

    Update CompressionEncoderMixin.java

--- src/main/java/tfar/xlpackets/mixin/CompressionEncoderMixin.java
+++ src/main/java/tfar/xlpackets/mixin/CompressionEncoderMixin.java
@@ -4,6 +4,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.network.CompressionEncoder;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.logging.PacketDump;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,33 +18,39 @@ import java.util.zip.Deflater;
 @Mixin(CompressionEncoder.class)
 public class CompressionEncoderMixin {
 
-    @Shadow
-    private int threshold;
-    @Final
-    @Shadow
-    private Deflater deflater;
-    @Final
-    @Shadow
-    private byte[] encodeBuf;
+    @Shadow @Final private static boolean DISABLE_PACKET_DEBUG;
+    @Shadow private int threshold;
+    @Shadow private Deflater deflater;
+    @Shadow private byte[] encodeBuf;
 
-    @Inject(method = "encode*", at = @At("HEAD"))
-    protected void encode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, ByteBuf byteBuf2, CallbackInfo info) {
+    @Shadow @Final private static Logger LOGGER;
+
+    @Inject(method = "encode*", at = @At("HEAD"), cancellable = true)
+    protected void encode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, ByteBuf byteBuf2, CallbackInfo ci) {
+        ci.cancel();
         int i = byteBuf.readableBytes();
-        FriendlyByteBuf friendlyByteBuf = new FriendlyByteBuf(byteBuf2);
+        FriendlyByteBuf friendlybytebuf = new FriendlyByteBuf(byteBuf2);
         if (i < this.threshold) {
-            friendlyByteBuf.writeVarInt(0);
-            friendlyByteBuf.writeBytes(byteBuf);
+            friendlybytebuf.writeVarInt(0);
+            friendlybytebuf.writeBytes(byteBuf);
         } else {
-            byte[] bs = new byte[i];
-            byteBuf.readBytes(bs);
-            friendlyByteBuf.writeVarInt(bs.length);
-            this.deflater.setInput(bs, 0, i);
+            if (!DISABLE_PACKET_DEBUG && i > 2000000000) {
+                byteBuf.markReaderIndex();
+                LOGGER.error("Attempted to send packet over maximum protocol size: {} > 2000000000\nData:\n{}", i, PacketDump.getContentDump(byteBuf));
+                byteBuf.resetReaderIndex();
+            }
+
+            byte[] abyte = new byte[i];
+            byteBuf.readBytes(abyte);
+            friendlybytebuf.writeVarInt(abyte.length);
+            this.deflater.setInput(abyte, 0, i);
             this.deflater.finish();
 
-            while (!this.deflater.finished()) {
+            while(!this.deflater.finished()) {
                 int j = this.deflater.deflate(this.encodeBuf);
-                friendlyByteBuf.writeBytes(this.encodeBuf, 0, j);
+                friendlybytebuf.writeBytes(this.encodeBuf, 0, j);
             }
+
             this.deflater.reset();
         }
     }
